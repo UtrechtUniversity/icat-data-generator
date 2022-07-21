d3be5995667bb454a057829ec3a7535cd8700ff4
Sietse Snel <s.t.snel@uu.nl>
2022-07-21 15:20:09 +0200

    Generated nested collections
    Collections are nested in case there are multiple batches.

--- icat_data_generate/iterators/coll_iterator.py
+++ icat_data_generate/iterators/coll_iterator.py
@@ -1,3 +1,5 @@
+import os
+
 from icat_data_generate.iterators.irods_iterator import IrodsIterator
 
 
@@ -11,7 +13,14 @@ class CollectionIterator(IrodsIterator):
     def __next__(self):
         if self.max_number == 0 or self.max_number > self.current:
             self.current += 1
-            return (next(self.collname_sample)[0], "/coll"+str(self.current),
+            coll_parent_name = next(self.collname_sample)[0]
+            while len(coll_parent_name) > 500:
+                # Keep parent collection names to a managable size so
+                # we don't exceed maximum path length
+                (coll_parent_name, _) = os.path.split(coll_parent_name)
+            coll_base_name = "coll"+str(self.current)
+            coll_full_name = os.path.join(coll_parent_name, coll_base_name)
+            return (coll_parent_name, coll_full_name,
                     next(self.owner_sample)[0], "testZone",
                     "1170000000", "1170000000")
         else:
